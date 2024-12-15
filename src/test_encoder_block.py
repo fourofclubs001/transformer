ad61064f2ace9501a867641fbd0ab895f52249f8
Lucas Vitali <lucasvitali001@gmail.com>
2024-12-15 14:14:28 -0300

    refactor: encoder

--- src/test_encoder_block.py
+++ src/test_encoder_block.py
@@ -8,28 +8,22 @@ class EncoderModuleTest(BaseTest):
 
         super().setUp()
 
+        self.encoder = Encoder(self.modelDimension)
+        self.input = torch.ones((1, self.sequenceLenght, self.modelDimension))
+        self.expected = torch.ones((1, self.sequenceLenght, self.modelDimension))
+
+    def assert_equal_dimensions(self, result: torch.Tensor, expected: torch.Tensor)-> None:
+
+        self.assertEqual(result.shape[0], self.expected.shape[0])
+        self.assertEqual(result.shape[1], self.expected.shape[1])
+        self.assertEqual(result.shape[2], self.expected.shape[2])
+
     def test_can_apply_attention(self):
 
-        input = torch.ones((1, self.sequenceLenght, self.modelDimension))
-
-        encoder = Encoder(self.modelDimension)
-        output = encoder.applyAttention(input)
-
-        expected = torch.ones((1, self.sequenceLenght, self.modelDimension))
-
-        self.assertEqual(output.shape[0], expected.shape[0])
-        self.assertEqual(output.shape[1], expected.shape[1])
-        self.assertEqual(output.shape[2], expected.shape[2])
+        output = self.encoder.applyAttention(self.input)
+        self.assert_equal_dimensions(output, self.expected)
 
     def test_can_do_pass_forward(self):
 
-        input = torch.ones((1, self.sequenceLenght, self.modelDimension))
-
-        encoder = Encoder(self.modelDimension)
-        output = encoder(input)
-
-        expected = torch.ones((1, self.sequenceLenght, self.modelDimension))
-
-        self.assertEqual(output.shape[0], expected.shape[0])
-        self.assertEqual(output.shape[1], expected.shape[1])
-        self.assertEqual(output.shape[2], expected.shape[2])
+        output = self.encoder(self.input)
+        self.assert_equal_dimensions(output, self.expected)
